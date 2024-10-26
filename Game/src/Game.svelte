7965b9a40e038b72c262044efae29fb97f076606
jespereklund <jespereklund@gmail.com>
2024-10-26 23:13:19 +0200

    flames

--- Game/src/Game.svelte
+++ Game/src/Game.svelte
@@ -1,8 +1,8 @@
 <script>
     import { onMount } from "svelte"
-    export let width = 800
-    export let height = 600
-    export let background = '#003300'
+    let canvasWidth = 800
+    let canvasHeight = 600
+    let background = '#003300'
 
     const shipSize = 100
     const drawTime = 20
@@ -24,22 +24,25 @@
     let timeString = "0.0 seconds"
     let startTime
     let timeRunning = false
+    let flameDirection
+    let flameOn = false
 
     onMount(async () => {
         drawTimer = setInterval(draw, drawTime)
         ctx = canvas.getContext('2d')
         document.onkeydown = checkKey
+        document.onkeyup = removeFlame
         ctx.font = "bold 100px Courier"
     })
 
     function draw() {
 
-        //outer walle
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        //outer walls
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight)
         ctx.fillStyle = "#006600"
         ctx.strokeStyle = "#00ff00"
         ctx.lineWidth = wallThickness
-        ctx.strokeRect(0, 0, canvas.width, canvas.height)
+        ctx.strokeRect(0, 0, canvasWidth, canvasHeight)
         
         //exit
         ctx.fillStyle = "#990000"
@@ -53,6 +56,11 @@
         //ship
         ctx.fillStyle = "#006600"
         ctx.fillRect(shipX, shipY, shipSize, shipSize)
+
+        //flame
+        if (flameOn) {
+            drawFlame(flameDirection)
+        }
 
         if (checkCompleted() === true) {
             shipXSpeed = 0
@@ -100,9 +108,9 @@
         if (
             //outer walls
             (shipX < wallThickness / 2 
-            || shipX + shipSize + wallThickness / 2 > canvas.width 
+            || shipX + shipSize + wallThickness / 2 > canvasWidth 
             || shipY < wallThickness / 2 
-            || shipY + shipSize + wallThickness / 2 > canvas.height ) 
+            || shipY + shipSize + wallThickness / 2 > canvasHeight ) 
             || 
             
             //Wall 1
@@ -124,28 +132,81 @@
     function checkKey(e) {
         if(timeRunning === false) {
             startTime = new Date()
+            timeRunning = true
         }
-        timeRunning = true
-
+        
+        //up
         if (e.keyCode == '38') {
+            startFlame("up")
             shipYSpeed -= acc
         }
+
+        //down
         else if (e.keyCode == '40') {
+            startFlame("down")
             shipYSpeed += acc
         }
+
+        //left
         else if (e.keyCode == '37') {
+            startFlame("left")
             shipXSpeed -= acc
         }
+
+        //right
         else if (e.keyCode == '39') {
+            startFlame("right")
             shipXSpeed += acc
         }
+    }
 
+    function startFlame(dir) {
+        flameDirection = dir
+        flameOn = true
+    }
+
+    function removeFlame() {
+        flameOn = false
+    }
+
+    function drawFlame(flameDirection) {
+        ctx.beginPath();
+        ctx.fillStyle = "#ff6600"
+        switch(flameDirection) {
+            case "up":
+                ctx.moveTo(shipX + shipSize / 2 - 10, shipY + shipSize);
+                ctx.lineTo(shipX + shipSize / 2, shipY + shipSize + Math.random() * 20 + 10);
+                ctx.lineTo(shipX + shipSize / 2 + 10, shipY + shipSize);
+                break;
+
+            case "down":
+                ctx.moveTo(shipX + shipSize / 2 - 10, shipY);
+                ctx.lineTo(shipX + shipSize / 2, shipY - Math.random() * 20 - 10);
+                ctx.lineTo(shipX + shipSize / 2 + 10, shipY);
+                break;
+
+            case "left":
+                ctx.moveTo(shipX + shipSize, shipY + shipSize / 2 - 10);
+                ctx.lineTo(shipX + shipSize + Math.random() * 20 + 10, shipY + shipSize / 2 );
+                ctx.lineTo(shipX + shipSize, shipY + shipSize / 2 + 10);
+                break;
+
+            case "right":
+                ctx.moveTo(shipX, shipY + shipSize / 2 - 10);
+                ctx.lineTo(shipX - Math.random() * 20 - 10, shipY + shipSize / 2 );
+                ctx.lineTo(shipX, shipY + shipSize / 2 + 10);
+                break;
+
+            default:
+                break;
+        }
+        ctx.fill();
     }
 </script>
 <main>
     <canvas
-        {width}
-		{height}
+        width={canvasWidth}
+		height={canvasHeight}
         style:background
         bind:this={canvas} 
     >
