f6fa68e1de3eb764b4afd07f17ee34eb69274fd2
Jesper Eklund <jespereklund@gmail.com>
2024-10-28 21:15:40 +0100

    start page

--- Game/src/App.svelte
+++ Game/src/App.svelte
@@ -1,11 +1,52 @@
 <script>
   import Game from "./Game.svelte";
+  let gameState = "start"
 </script>
 <main>
-  <Game></Game>
+	{#if gameState === "start"}
+		<div class="start-div">
+			<p class="text">Du skal styre det lille blå rumskib hen til den røde udgang. Rører du væggene undervejs, dør du!<br><br>Brug piletasterne eller WASD til at styre rumskibet.</p>
+			<br><br>
+			<p class="text"><button on:click={(e) => gameState = "game"}>Start Game</button></p>
+		</div>
+	{:else if gameState === "game" }
+		<Game></Game>
+	{/if}
 </main>
 
 <style>
+
+	button {
+		width: 230px;
+		height: 70px;
+		font-family: 'Courier New', Courier, monospace;
+		font-size: 30px;
+		font-weight: bolder;
+		color: #00ff00;
+		background: #004400;
+		border: 3px solid #00ff00;
+		cursor: pointer;
+	}
+
+	.text {
+		font-family: 'Courier New', Courier, monospace;
+		font-size: 40px;
+		font-weight: bolder;
+		color: #009900;
+		margin: 0;
+		text-align: center;
+	}
+
+	.start-div {
+		width: 800px; 
+		height: 600px; 
+		background-color: #003300;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		border: 10px solid #00ff00;
+	}
 	:global(body) {
 		margin: 0;
 		padding: 0;
